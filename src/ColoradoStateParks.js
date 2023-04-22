ea84f773ec25116c219c2c7bc1edca2a602c0587
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-04-22 16:03:10 -0400

    commit tree abba256368a22ded43c4c991801a22d676cd66e4

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from './parks/RockyMountain'
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  return (
+    <div>
+      
+      <MesaVerde />
 
-  return <h1>Colorado State Parks!</h1>;
+    </div>
+  )
 }
+
+export default ColoradoStateParks
